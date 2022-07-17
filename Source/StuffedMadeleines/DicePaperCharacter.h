12fbd33461685756fca81dce37a4de245b6712ff
SIL3nCe <tom.pradal@aliceadsl.fr>
2022-07-18 00:19:44 +0200

    Imported from p4

---
+++ Source/StuffedMadeleines/DicePaperCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "DicePaperCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STUFFEDMADELEINES_API ADicePaperCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
